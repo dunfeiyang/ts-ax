cd4f2e2254f0cf7bc1c69d718c94412c3cd2afe4
dunfeiyang <dfylk@qq.com>
2020-03-03 20:24:17 +0800

    feat: server
    server and webpack

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Methods =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Methods
+  data?: any
+  params?: any
+}
---
+++ src/xhr.ts
@@ -0,0 +1,11 @@
+import { AxiosRequestConfig } from './types'
+
+export default function xhr(config: AxiosRequestConfig): void {
+  const { data = null, url, method = 'get' } = config
+
+  const request = new XMLHttpRequest()
+
+  request.open(method.toLocaleLowerCase(), url, true)
+
+  request.send(data)
+}
